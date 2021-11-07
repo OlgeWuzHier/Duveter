2b28ce93cd11653d94fd1c7957733211c76fae75
Olga <succikae@gmail.com>
2021-11-07 20:32:30 +0100

    database testing

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
+from dotenv import load_dotenv
 from flask import Flask
+from flask_pymongo import PyMongo 
+# from PyMongo import MongoClient
+import os
+from bson import json_util
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.getenv("CONNECTION_STRING")
+mongo = PyMongo(app)
 
 @app.route("/")
 def helloworld():
-    return "Hello world"
+    x = mongo.db.test.find_one({"t1": "123"})
+    return json_util.dumps(x)
